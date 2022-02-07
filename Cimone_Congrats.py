f6a014df1b0e213dbee7b5c2caa7d7b48245c4f5
ku3nsting1 <ku3nsting@users.noreply.github.com>
2022-02-07 18:00:31 +0000

    Added random congrats generator

--- Cimone_Congrats.py
+++ Cimone_Congrats.py
@@ -1,4 +1,9 @@
+import random
+
+arr = ["proud of you!", "happy for you!", "excited!"]
+rand = random.randrange(0, 3)
+
 print("*** *** *** *** ***")
 print("Congratulations Cimone!")
 print("*** *** *** *** ***")
-print("We're proud of you!")
+print(f"We're {arr[rand]}")
